b80f63813ae571b039dfeb05292ef016baea806c
Keely Jones <keelyjmjones@gmail.com>
2020-04-10 14:43:26 +0000

    added location data to markers.js; started to create functions for location filtering in map.js; removed inbuilt google search box from maps.js and associated CSS; updated new search filtering in index.html

--- scripts/map.js
+++ scripts/map.js
@@ -1,119 +1,154 @@
+var infoWindow, map, places, photo;
+var markers = [];
+var autocomplete;
+// var hostnameRegexp = new RegExp('^https?://.+?/');
+var museum_icon = "assets/mapMarkers/museums.png";
+var park_icon = "assets/mapMarkers/museums.png";
+var restaurant_icon = "assets/mapMarkers/restaurants.png";
+var hotel_icon = "assets/mapMarkers/hotels.png";
+
 function initMap() {
-     var mapOptions = {
+    var mapOptions = {
         zoom: 14,
         center: { lat: 51.752021, lng: -1.257726 },
         mapTypeId: 'roadmap'
     };
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    
-    // Create markers:  
-    let icons = {
-            museums: {
-                icon: '/assets/mapMarkers/museums.png'
-            },
-            attractions: {
-                icon: 'assets/mapMarkers/default.png'
-            },
-            parks: {
-                icon: '/assets/mapMarkers/parks.png'
-            },
-            colleges: {
-                icon: '/assets/mapMarkers/schools.png'
+
+    function searchPlaces() {
+        // Get data from the desired location.
+        const inputSelectGroup = document.getElementById("input-select-group").value;
+        if (inputSelectGroup == 1) {
+            var search = {
+                bounds: map.getBounds(),
+                types: ['museum'],
+
+            };
+        } else if (inputSelectGroup == 2) {
+            search = {
+                bounds: map.getBounds(),
+                types: ['park'],
+
+            };
+        } else if (inputSelectGroup == 3) {
+            search = {
+                bounds: map.getBounds(),
+                types: ['restaurant'],
+
+            };
+        } else if (inputSelectGroup == 4) {
+            search = {
+                bounds: map.getBounds(),
+                types: ['lodging']
+            };
+        }
+    }
+
+    places.nearbySearch(search, function (results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            clearResults();
+            clearMarkers();
+            // Create a marker for each place found
+            for (var i = 0; i < results.length; i++) {
+                // Use marker animation to drop the icons incrementally on the map.
+                markers[i] = new google.maps.Marker({
+                    position: results[i].geometry.location,
+                    animation: google.maps.Animation.DROP,
+                    icon: null
+                });
+                // If the user clicks on a marker, show details of that place
+                // in an info window.
+                markers[i].placeResult = results[i];
+                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+                setTimeout(dropMarker(i), i * 100);
+                addResult(results[i], i);
+                // Set icons for markers on the map 
+                if (inputSelectGroup == 1) {
+                    markers[i].icon = museum_icon
+                } else if (inputSelectGroup == 2) {
+                    markers[i].icon = park_icon
+                } else if (inputSelectGroup == 3) {
+                    markers[i].icon = restaurant_icon
+                } else {
+                    if (inputSelectGroup == 3) {
+                        markers[i].icon = hotel_icon
+                    }
+                }
+
             }
-        };  
-    let markerData = [
-        {
-            position: new google.maps.LatLng(51.755213, -1.258065),
-            type: 'colleges',
-            title: 'Balliol College'
-        }, {
-            position: new google.maps.LatLng(51.752935, -1.255150),
-            type: 'colleges',
-            title: 'Brasenose College'
-        }, {
-            position: new google.maps.LatLng(51.750053, -1.255630),
-            type: 'colleges',
-            title: 'Christ Church College'
-        }, {
-            position: new google.maps.LatLng(51.751060, -1.253772),
-            type: 'colleges',
-            title: 'Corpus Christi College'
-        }, {
-            position: new google.maps.LatLng(51.771858, -1.285064),
-            type: 'parks',
-            title: 'Port Meadow'
-        }, {
-            position: new google.maps.LatLng(51.762456, -1.253113),
-            type: 'parks',
-            title: 'University Parks'
-        }, {
-            position: new google.maps.LatLng(51.751666, -1.246910),
-            type: 'parks',
-            title: 'Botanic Gardens'
-        }, {
-            position: new google.maps.LatLng(51.748204, -1.251124),
-            type: 'parks',
-            title: 'Christ Church Meadows'
-        }, {
-            position: new google.maps.LatLng(51.752352, -1.229344),
-            type: 'parks',
-            title: 'South Park'
-        }, {
-            position: new google.maps.LatLng(51.758809, -1.255478),
-            type: 'museums',
-            title: 'Natural History Museum'
-        }, {
-            position: new google.maps.LatLng(51.758763, -1.255939),
-            type: 'museums',
-            title: 'Pitt Rivers Museum'
-        }, {
-            position: new google.maps.LatLng(51.755555, -1.259909),
-            type: 'museums',
-            title: 'Ashmolean museum'
-        }, {
-            position: new google.maps.LatLng(51.754434, -1.255146),
-            type: 'museums',
-            title: 'History of Science Museum'
-        }, {
-            position: new google.maps.LatLng(51.748988, -1.255927),
-            type: 'museums',
-            title: 'Bate Collection of Musical Instruments'
-        }, {
-            position: new google.maps.LatLng(51.751232, -1.257071),
-            type: 'museums',
-            title: 'Museum of Oxford'
-        }, {
-            position: new google.maps.LatLng(51.750857, -1.257664),
-            type: 'museums',
-            title: 'The Story Museum'
-        }, {
-            position: new google.maps.LatLng(51.754259, -1.253731),
-            type: 'attractions',
-            title: 'Bodleian Library'
-        }, {
-            position: new google.maps.LatLng(51.751841, -1.262995),
-            type: 'attractions',
-            title: 'Oxford Castle & Prison'
-        }, {
-            position: new google.maps.LatLng(51.752981, -1.256355),
-            type: 'attractions',
-            title: 'Covered Market'
-        }, {
-            position: new google.maps.LatLng(51.752064, -1.257653),
-            type: 'attractions',
-            title: 'Carfax Tower'
-        }, {
-            position: new google.maps.LatLng(51.755436, -1.258309),
-            type: 'attractions',
-            title: 'Martyrs Memorial'
-        }, 
-    ];
-    for (var i = 0; i < markerData.length; i++) {
-        var marker = new google.maps.Marker({
-            position: markerData[i].position,
-            icon: icons[markerData[i].type].icon,
-            map: map,
-            title: markerData[i].title
-        });
-    };
+        }
+    });
+
+    function clearMarkers() {
+        for (var i = 0; i < markers.length; i++) {
+            if (markers[i]) {
+                markers[i].setMap(null);
+            }
+        }
+        markers = [];
+    }
+
+    function dropMarker(i) {
+        return function () {
+            markers[i].setMap(map);
+        };
+    }
+
+
 }
+
+
+infoWindow = new google.maps.InfoWindow({
+    content: document.getElementById('info-content')
+});
+
+places = new google.maps.places.PlacesService(map);
+autocomplete = new google.maps.places.Autocomplete((
+    document.getElementById('search-places')));
+
+let geocoder = new google.maps.Geocoder();
+document.getElementById('button').addEventListener('click', function () {
+    geocodeAddress(geocoder, map);
+});
+
+// Converts addresses like (street address, city, town etc ) into geographic coordinates (like latitude and longitude).
+function geocodeAddress(geocoder) {
+    const address = document.getElementById('search-places').value;
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status === 'OK') {
+            map.fitBounds(results[0].geometry.viewport);
+
+            searchPlaces();
+        } else {
+            $("#enter-city").html(`<h3 class = "enter-input">Please enter an Valid Location!</h3>`);
+            $("#map").removeClass("map");
+            document.getElementById("background-image-container").style.height = "100vh";
+        }
+    });
+}
+
+function showInfoWindow() {
+
+    let marker = this;
+    places.getDetails({ placeId: marker.placeResult.place_id },
+
+        function (place, status) {
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                return;
+            }
+            infoWindow.open(map, marker);
+            buildIWContent(place);
+
+        });
+}
+
+//    let locations = filterByType(type)
+
+//    function updateCategory() {
+//        let type = select.Value;
+//    };
+
+// bind the location.type (from JS) to each select option (in index.html) in the dropdown
+// create a function that onClick the map shows the locations of that type
+// three things that needs to speak to each other - JS types speak to map, select speaks to map which filters the JS data
+
